ae3d6d29f4ff06ad18ecd12b8c6acc6409d6e40b
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-06 13:16:42 +0530

    commit

--- bookshopmaintenanceapplication/mysql-pact/sql-scripts/InsertBooks.sql
+++ bookshopmaintenanceapplication/mysql-pact/sql-scripts/InsertBooks.sql
@@ -1,2 +1,2 @@
-INSERT INTO bookdetails (book_name, book_price , publishing_year) VALUES ('Spring', '333', '2008');
-INSERT INTO bookdetails (book_name, book_price , publishing_year) VALUES ('Head First Java', '498', '2005');
+INSERT INTO bookdetails (book_name, book_price , publisher_name, publishing_year) VALUES ('Spring', '333','raj','2008');
+INSERT INTO bookdetails (book_name, book_price ,  publisher_name,publishing_year) VALUES ('Head First Java', '498','mehtha','2005');
