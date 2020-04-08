bf9f035d26033ede0c3f303e59d3e24076853f3a
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-08 16:51:14 +0530

    Commit

--- bookshopmaintenanceapplication/src/test/java/com/bookshopapp/BookShopMaintenanceApplicationTests.java
+++ bookshopmaintenanceapplication/src/test/java/com/bookshopapp/BookShopMaintenanceApplicationTests.java
@@ -1,24 +1,31 @@
-ackage com.bookshopapp;
-
-import java.util.Arrays;
-import org.apache.http.HttpRequest;
+package com.bookshopapp;
 
 import org.junit.BeforeClass;
+
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.context.ConfigurableApplicationContext;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.bookshopapp.model.repository.BookRepo;
 
 import au.com.dius.pact.provider.junit.Provider;
+
 import au.com.dius.pact.provider.junit.VerificationReports;
+
 import au.com.dius.pact.provider.junit.loader.PactFolder;
+
 import au.com.dius.pact.provider.junit.target.HttpTarget;
+
 import au.com.dius.pact.provider.junit.target.Target;
+
 import au.com.dius.pact.provider.junit.target.TestTarget;
 
 @RunWith(SpringRunner.class)
@@ -37,14 +44,17 @@ public class BookShopMaintenanceApplicationTests {
 
 	@BeforeClass
 	public static void start() {
+		
 		System.setProperty("pact.provider.version", "94");
+		
 		ConfigurableApplicationContext context = SpringApplication.run(BookShopMaintenanceApplication.class);
+		
 		repo = context.getBean(BookRepo.class);
 
 	}
 
 	@Test
 	public void contextLoads() {
+		
 	}
-
 }
