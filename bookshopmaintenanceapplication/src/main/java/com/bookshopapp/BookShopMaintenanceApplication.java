b2a1c5743a95ac83bffe19a86abd1a53286078e4
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-08 16:48:59 +0530

    Commit

--- bookshopmaintenanceapplication/src/main/java/com/bookshopapp/BookShopMaintenanceApplication.java
+++ bookshopmaintenanceapplication/src/main/java/com/bookshopapp/BookShopMaintenanceApplication.java
@@ -1,9 +1,14 @@
 package com.bookshopapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.bookshopapp.model.entities.Book;
 
 import com.bookshopapp.model.repository.BookRepo;
 
@@ -11,6 +16,7 @@ import com.bookshopapp.model.repository.BookRepo;
 public class BookShopMaintenanceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(BookShopMaintenanceApplication.class, args);
 	}
 
@@ -19,10 +25,11 @@ public class BookShopMaintenanceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-	        /*
-		 * Book b1 = new Book("Head First Java", 498, "Raj", 2005); bookRepo.save(b1);
-		 * Book b2= new Book("Spring", 333, "Mhetha", 2008); bookRepo.save(b2);
+        /*
+		 * Book b1 = new Book("Head First Java", 498, "Raj", 2005); 
+		 * bookRepo.save(b1);
+		 * Book b2= new Book("Spring", 333, "Mhetha", 2008);
+		 *  bookRepo.save(b2);
 		 */
-	}
+    }
 }
