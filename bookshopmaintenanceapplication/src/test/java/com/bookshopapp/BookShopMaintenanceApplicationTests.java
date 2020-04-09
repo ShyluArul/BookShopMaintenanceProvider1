a49fca65c493f2cf9b6a4985f1ea92ec395abdff
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-09 13:14:28 +0530

    Commit

--- bookshopmaintenanceapplication/src/test/java/com/bookshopapp/BookShopMaintenanceApplicationTests.java
+++ bookshopmaintenanceapplication/src/test/java/com/bookshopapp/BookShopMaintenanceApplicationTests.java
@@ -1,31 +1,17 @@
 package com.bookshopapp;
-
 import org.junit.BeforeClass;
-
 import org.junit.Test;
-
 import org.junit.runner.RunWith;
-
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.springframework.context.ConfigurableApplicationContext;
-
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.bookshopapp.model.repository.BookRepo;
-
 import au.com.dius.pact.provider.junit.Provider;
-
 import au.com.dius.pact.provider.junit.VerificationReports;
-
 import au.com.dius.pact.provider.junit.loader.PactFolder;
-
 import au.com.dius.pact.provider.junit.target.HttpTarget;
-
 import au.com.dius.pact.provider.junit.target.Target;
-
 import au.com.dius.pact.provider.junit.target.TestTarget;
 
 @RunWith(SpringRunner.class)
@@ -34,27 +20,19 @@ import au.com.dius.pact.provider.junit.target.TestTarget;
 @VerificationReports({ "console", "markdown", "json" })
 @SpringBootTest
 public class BookShopMaintenanceApplicationTests {
-
-	private static BookRepo repo;
-
-	private int PORT = 8090;
-
-	@TestTarget
+        private static BookRepo repo;
+        private int PORT = 8090;
+        @TestTarget
 	public final Target target = new HttpTarget("http", "localhost", PORT);
-
-	@BeforeClass
+	
+        @BeforeClass
 	public static void start() {
-		
 		System.setProperty("pact.provider.version", "94");
-		
 		ConfigurableApplicationContext context = SpringApplication.run(BookShopMaintenanceApplication.class);
-		
 		repo = context.getBean(BookRepo.class);
-
-	}
-
-	@Test
+        }
+	
+        @Test
 	public void contextLoads() {
-		
 	}
 }
