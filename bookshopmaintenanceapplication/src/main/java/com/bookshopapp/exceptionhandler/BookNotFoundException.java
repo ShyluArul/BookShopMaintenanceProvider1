88d752361a92986e060391dadb03aba55347cd9a
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-09 13:39:52 +0530

    Update Commit

--- bookshopmaintenanceapplication/src/main/java/com/bookshopapp/exceptionhandler/BookNotFoundException.java
+++ bookshopmaintenanceapplication/src/main/java/com/bookshopapp/exceptionhandler/BookNotFoundException.java
@@ -4,5 +4,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
+     private static final long serialVersionUID = 1L;
 }
