527a2de3dbb4d8a54dbf4b53921ba51ae02f382e
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-09 13:05:45 +0530

    Commit

--- bookshopmaintenanceapplication/src/main/java/com/bookshopapp/model/repository/BookRepo.java
+++ bookshopmaintenanceapplication/src/main/java/com/bookshopapp/model/repository/BookRepo.java
@@ -1,18 +1,11 @@
 package com.bookshopapp.model.repository;
-
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
-
 import com.bookshopapp.model.entities.Book;
 
 @Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
-
-	public Optional<Book> findBybookName(String bookName);
-
-	public Optional<Book> findBookById(Long id);
-
+       public Optional<Book> findBybookName(String bookName);
+       public Optional<Book> findBookById(Long id);
 }
