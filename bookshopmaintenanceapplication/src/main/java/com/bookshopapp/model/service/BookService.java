fa7ae26ecc0617cd2c7707f74577a8489485b1f6
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-09 13:07:09 +0530

    Commit

--- bookshopmaintenanceapplication/src/main/java/com/bookshopapp/model/service/BookService.java
+++ bookshopmaintenanceapplication/src/main/java/com/bookshopapp/model/service/BookService.java
@@ -1,24 +1,12 @@
 package com.bookshopapp.model.service;
-
 import java.util.List;
-
 import java.util.Optional;
-
 import com.bookshopapp.model.entities.Book;
-
 public interface BookService {
-	
 	public List<Book> getAllBooks();
-
-	public Optional<Book> findBybookName(String bookName);
-
-	public Optional<Book> findBookById(Long id);
-
-	public void deleteBook(Long id);
-
-	public Book addBook(Book book);
-
-	public Book updateBook(Long id, Book book);
-
+        public Optional<Book> findBybookName(String bookName);
+        public Optional<Book> findBookById(Long id);
+        public void deleteBook(Long id);
+        public Book addBook(Book book);
+        public Book updateBook(Long id, Book book);
 }
-
