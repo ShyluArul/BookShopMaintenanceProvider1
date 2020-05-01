d45e8f7fba84ccbe6e04e3768f898fdb88d9da5b
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-05-01 21:45:22 +0530

    updatebookshopprovidertest

--- bookshopmaintenanceapplication/src/test/java/com/bookshopapp/BookShopMaintenanceApplicationTests.java
+++ bookshopmaintenanceapplication/src/test/java/com/bookshopapp/BookShopMaintenanceApplicationTests.java
@@ -1,4 +1,5 @@
 package com.bookshopapp;
+
 import java.util.Arrays;
 import java.util.Map;
 import org.apache.http.HttpRequest;
@@ -19,44 +20,51 @@ import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
+
 @RunWith(PactRunner.class)
 @PactFolder("../acceptedPacts")
 @Provider("mybookservice")
-@VerificationReports({"console", "markdown", "json"})
+@VerificationReports({ "console", "markdown", "json" })
 @IgnoreNoPactsToVerify
 public class BookShopMaintenanceApplicationTests {
 	private int PORT = 8090;
 
 	@TestTarget
 	public final Target target = new HttpTarget("http", "localhost", PORT);
+
 	private static ConfigurableApplicationContext applicationContext;
 
 	@BeforeClass
-	public static void start() {
-		System.setProperty("pact.provider.version", "94");
-		SpringApplication.run(BookShopMaintenanceApplication.class);
+	public static void setVersions() {
+		System.setProperty("pact.provider.version", "1.0.0");
+		System.setProperty("pact.verifier.publishResults", "true");
 	}
-	
-        // This method acts as an interceptor before the validation happens
+
+	@BeforeClass
+	public static void start() {
+		applicationContext = SpringApplication.run(BookShopMaintenanceApplication.class);
+	}
+
+	// This method acts as an interceptor before the validation happens
 	// so if the provider needs to inject andything in the header or body
 	// like access token,encrypted password can be done in a secured way
 	// without putting the secret/access token in the pact document
 	@TargetRequestFilter
 	public void printTheRequestHeaders(HttpRequest request) {
 		Arrays.asList(request.getAllHeaders())
-				.forEach(header -> System.out.println(header.getName() + "->" + header.getValue()));
+			.forEach(header -> System.out.println(header.getName() + "->" + header.getValue()));
 	}
 
 	@State("SomeState")
 	public void withSomeState() {
-		System.out.println("do something with state");
+		System.out.println("something with state");
 		BookService bookservice = applicationContext.getBean(BookService.class);
 		bookservice.addBook(new Book("C++", 324, "Manoj", 2001));
 	}
 
 	@State("SomeStateWithMap")
 	public void withSomeStateAndMap(Map<String, Object> params) {
-		System.out.println("do something with state");
+		System.out.println("something with state");
 		params.forEach((key, value) -> System.out.println(key + " -> " + value));
 		BookService bookservice = applicationContext.getBean(BookService.class);
 		@SuppressWarnings("unchecked")
